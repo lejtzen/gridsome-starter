ef4adbeb04ec951f2e079ea1fc1f8e0a83c89883
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-10-11 20:21:33 +0200

    Add custom App template

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    const metaInfo = { meta: [] }
+    const { siteName, siteDescription } = this.$static.metadata
+
+    if (siteName) {
+      metaInfo.title = siteName
+    }
+
+    if (siteDescription) {
+      metaInfo.meta.push({
+        key: 'description',
+        name: 'description',
+        content: siteDescription,
+      })
+    }
+
+    return metaInfo
+  },
+}
+</script>
+
+<static-query>
+    query {
+        metadata {
+            siteName
+            siteDescription
+        }
+    }
+</static-query>
